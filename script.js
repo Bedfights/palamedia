6486152f727454ad27f0a932388491bb4fa78d0c
Bedfights <albin.jornevald2007@gmail.com>
2025-11-19 19:38:18 +0100

    just work now

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const menuButton = document.getElementById('menuButton');
 menuButton.onclick = function() {
     menu.style.display = 'block';
 }
+if (location.hostname !== "bedfights.github.io") {
+    document.querySelectorAll("a").forEach(a => {
+        a.href = a.href.replace("/palamedia/", "/");
+    });
+}
+console.log(location.hostname);
